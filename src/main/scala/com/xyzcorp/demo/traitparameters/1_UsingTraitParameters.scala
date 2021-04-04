703ce792d3219fb8ef88e9595f08438bdbfea1e2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-04-04 17:03:14 -0600

    created better trait parameter example

--- src/main/scala/com/xyzcorp/demo/traitparameters/1_UsingTraitParameters.scala
+++ src/main/scala/com/xyzcorp/demo/traitparameters/1_UsingTraitParameters.scala
@@ -11,37 +11,16 @@ package com.xyzcorp.demo.traitparameters
 * Traits offer I am "adding in a feature"
 **/
 
-enum DebugLevel:
-  case FATAL, ERROR, WARN, INFO, DEBUG, TRACE, OFF
+trait Transaction(amt:Int, currency:String):
+  def log:String = 
+     s"Transaction of $amt $currency was processed"
 
-case class LogItem(level:DebugLevel, msg:String)
+case class Deposit(amt:Int, currency:String) extends Transaction(amt, currency)
+case class Withdrawal(amt:Int, currency:String) extends Transaction(amt, currency)
 
-trait DebugThreshold(threshold:DebugLevel)
+@main def assertThatTraitCanContainParams:Unit = 
+  val deposit = Deposit(100, "Yen")
+  println(deposit.log)
 
-trait LogStack(depth:Int) extends DebugThreshold: 
-  import scala.collection.mutable.Queue
-  private val queue = Queue.empty[LogItem]
-  def add(logItem:LogItem) =
-    if logItem.level.ordinal <= threshold.ordinal then
-      queue.enqueue(logItem)
-    if queue.size > depth then queue.dequeue
-  def elements = queue.toList
-
-case class Account(accountNumber: String, balance:Long) extends LogStack(5) with DebugThreshold(DebugLevel.WARN):
-  def deposit(amount:Long) = 
-    add(LogItem(DebugLevel.DEBUG, s"Depositing $amount"))
-  def withdrawal(amount:Long) = 
-    add(LogItem(DebugLevel.DEBUG, s"Attempt withdrawal of $amount"))
-    if !(balance - amount < 0) then 
-      add(LogItem(DebugLevel.DEBUG,s"Withdrawal $amount successful"))
-
-@main def assertThatWeCanUseTraitParameters:Unit = 
-  val account = Account("12-40-590903", 1203)
-  println(account.elements)
-  account.deposit(40)
-  account.deposit(400)
-  account.withdrawal(100)
-  account.withdrawal(1200)
-  account.withdrawal(1300)
-  account.withdrawal(1400)
-  println(account.elements)
+  val deposit2 = Deposit(500, "Euro")
+  println(deposit2.log)
